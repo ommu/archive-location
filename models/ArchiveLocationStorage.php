49aa2ce93261877d950ce50b2cdb1dd80546312f
Putra Sudaryanto <putra.sudaryanto@gmail.com>
2020-02-20 10:33:58 +0700

    change SerialColumn use Class

--- models/ArchiveLocationStorage.php
+++ models/ArchiveLocationStorage.php
@@ -180,7 +180,7 @@ class ArchiveLocationStorage extends \app\components\ActiveRecord
 
 		$this->templateColumns['_no'] = [
 			'header' => '#',
-			'class' => 'yii\grid\SerialColumn',
+			'class' => 'app\components\grid\SerialColumn',
 			'contentOptions' => ['class'=>'text-center'],
 		];
 		$this->templateColumns['parentName'] = [
